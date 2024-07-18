799a5edbd8cb56c709db7c70dab170868232533b
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-07-17 22:37:08 -0300

    eliminacion de comentarios y espacios

--- src/movie/movie.entity.ts
+++ src/movie/movie.entity.ts
@@ -1,34 +1,35 @@
-import { Collection, Entity, ManyToMany, OneToMany, Property } from "@mikro-orm/core";
-import { BaseEntity } from "../shared/db/baseEntity.entity.js";
-import { Genre } from "../genre/genre.entity.js";
-import { Cinema } from "../cinema/cinema.entity.js";
-import { Show } from "../show/show.entity.js";
+import {
+  Collection,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  Property,
+} from '@mikro-orm/core';
+import { BaseEntity } from '../shared/db/baseEntity.entity.js';
+import { Genre } from '../genre/genre.entity.js';
+import { Cinema } from '../cinema/cinema.entity.js';
+import { Show } from '../show/show.entity.js';
 
 @Entity()
 export class Movie extends BaseEntity {
-
-  @Property({ nullable: false }) // no le pongo unique porque hay peliculas de mismo nombre
-  name!: string
+  @Property({ nullable: false })
+  name!: string;
 
   @Property({ nullable: false })
-  description!: string
+  description!: string;
 
   @Property({ nullable: false })
-  format!: string
+  format!: string;
 
   @ManyToMany(() => Genre, (genre) => genre.movies)
-  genres = new Collection<Genre>(this)
+  genres = new Collection<Genre>(this);
 
-  /* por las dudas lo dejamos durmiendo por ahora
+  /*
 
   @ManyToMany(()=> Cinema), (cinema) => cinema.movies)
   cinemas = new Collection<Cinema>(this) 
   */
 
-
-  //Relacion con Funcion ---------------- Modificado por Sabrina.
-  
-  @OneToMany(() => Show, (show) => show.movie )
-  shows = new Collection<Show>(this)
-  
+  @OneToMany(() => Show, (show) => show.movie)
+  shows = new Collection<Show>(this);
 }
--- src/theater/theater.entity.ts
+++ src/theater/theater.entity.ts
@@ -1,22 +1,24 @@
-import { Cascade, Collection, Entity, ManyToOne, OneToMany, Property, Rel } from '@mikro-orm/core';
+import {
+  Cascade,
+  Collection,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  Property,
+  Rel,
+} from '@mikro-orm/core';
 import { BaseEntity } from '../shared/db/baseEntity.entity.js';
 import { Cinema } from '../cinema/cinema.entity.js';
 import { Show } from '../show/show.entity.js';
 
-
 @Entity()
 export class Theater extends BaseEntity {
-
   @Property({ nullable: false })
   numChairs!: number;
 
   @ManyToOne(() => Cinema, { nullable: false })
   cinema!: Rel<Cinema>;
 
-  
-  //Relacion con Funcion ---------------- Modificado por Sabrina.
-  
-  @OneToMany(() => Show, (show) => show.theater, {cascade: [Cascade.ALL]})
+  @OneToMany(() => Show, (show) => show.theater, { cascade: [Cascade.ALL] })
   shows = new Collection<Show>(this);
-
 }
