302f736523d809acb5ef63c1b1cd0a24d944fc17
Santino S <santinosimbel@gmail.com>
2024-07-26 13:27:18 -0300

    Simplificacion de checkTimeShowInTheather

--- src/utils/checkTimeShowInTheater.ts
+++ src/utils/checkTimeShowInTheater.ts
@@ -4,33 +4,26 @@ import { Show } from "../show/show.entity.js"
 
 //true si se encuentra una funcion dentro del mismo horario
 //false si no se encuentra una funcion entre eso horario
-export async function  checkTimeShowInTheater(dayAndTime:string, finishTime:string, theaterId:string):Promise<Boolean>{
+export async function checkTimeShowInTheater(dayAndTime: string, finishTime: string, theaterId: string): Promise<Boolean> {
   const em = orm.em
   const newtheaterId = Number(theaterId);
-  const startDate = new Date(`${dayAndTime}Z`);
-  const endDate = new Date(`${finishTime}Z`);
-  try{
+  const newShowStartDate = new Date(`${dayAndTime}Z`);
+  const newShowEndDate = new Date(`${finishTime}Z`);
+  try {
     const overlappingShows = await em.find(Show, {
       theater: newtheaterId,
-      $or: [
-              { 
-                dayAndTime: { $lt: endDate }, 
-                finishTime: { $gt: startDate }
-              },
-              { 
-                dayAndTime: { $lte: endDate }, 
-                finishTime: { $gte: startDate }
-              }
-            ]
+      dayAndTime: { $lt: newShowEndDate },
+      finishTime: { $gt: newShowStartDate }
     });
+
     if (overlappingShows.length > 0) {
       return true;
     } else {
       return false;
     }
-  }catch(error){
+  } catch (error) {
     console.error("Error checking for overlapping shows:", error);
     return true;
   }
-  
+
 }
