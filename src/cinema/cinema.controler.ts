faf9c4ebc47dbcc56bbe685a992f7e16a12270a0
Santino S <santinosimbel@gmail.com>
2024-09-11 22:18:42 -0300

    Arreglo y simplificacion de remove en cinema

--- src/cinema/cinema.controler.ts
+++ src/cinema/cinema.controler.ts
@@ -21,7 +21,7 @@ function sanitizeCinemaInput(req: Request, res: Response, next: NextFunction) {
 
 async function findAll(req: Request, res: Response) {
   try {
-    const cinemas = await em.find(Cinema,{},{ populate: ['theaters', 'movies'] });
+    const cinemas = await em.find(Cinema, {}, { populate: ['theaters', 'movies'] });
     res.status(200).json({ message: 'found all cinemas', data: cinemas });
   } catch (error: any) {
     res.status(500).json({
@@ -34,7 +34,7 @@ async function findAll(req: Request, res: Response) {
 async function findOne(req: Request, res: Response) {
   try {
     const id = Number.parseInt(req.params.id);
-    const cinema = await em.findOne(Cinema,{ id },{ populate: ['theaters', 'movies'] });
+    const cinema = await em.findOne(Cinema, { id }, { populate: ['theaters', 'movies'] });
     if (cinema === null) {
       res.status(404).json({ message: 'cinema not found' });
     } else {
@@ -83,13 +83,12 @@ async function update(req: Request, res: Response) {
 async function remove(req: Request, res: Response) {
   try {
     const id = Number.parseInt(req.params.id);
-    const cinemaToRemove = em.getReference(Cinema, id);
     const cinema = await em.findOne(Cinema, { id }, { populate: ['theaters'] });
     if (cinema === null) {
       res.status(404).json({ message: 'cinema not found to delete.' });
     } else {
-      await em.removeAndFlush(cinemaToRemove);
-      res.status(200).json({ message: 'cinema deleted' });
+      await em.removeAndFlush(cinema);
+      res.status(200).json({ data: cinema, message: 'cinema deleted' });
     }
   } catch (error: any) {
     res.status(500).json({
