65bef9992dd3736539c0e98cecbd0e11e8557c3b
Santino S <santinosimbel@gmail.com>
2024-09-26 22:10:14 -0300

    Arreglo en movies.routes para los proximos estrenos

--- src/movie/movie.routes.ts
+++ src/movie/movie.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { sanitizeMovieInput, findAll, findOne, add, update, remove, findNextMoviesReleases } from "./movie.controler.js";
-// importar findNextMoviesReleases
 
 export const movieRouter = Router()
 
 movieRouter.get('/', findAll)
+movieRouter.get('/next-releases', findNextMoviesReleases) //Cuidado con el orden, como estaba abajo del findOne tomaba /next-releases como id y por eso fallaba.
 movieRouter.get('/:id', findOne)
-movieRouter.get('/next-releases', findNextMoviesReleases)
 movieRouter.post('/', sanitizeMovieInput, add)
 movieRouter.put('/:id', sanitizeMovieInput, update)
 movieRouter.patch('/:id', sanitizeMovieInput, update)
