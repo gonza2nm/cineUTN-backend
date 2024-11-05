d6181316f4567d256b85a2aaf01cac274b76a858
Santino S <santinosimbel@gmail.com>
2024-11-05 00:48:20 -0300

    Arreglo de rutas y agregado las de manager

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -6,13 +6,22 @@ import {
   add,
   update,
   remove,
+  findOneManager,
+  findAllManagers,
 } from './user.controler.js';
 
 export const userRouter = Router();
 
+
 userRouter.get('/', findAll);
-userRouter.post('/login', sanitizeUserInput, findOne);
-userRouter.post('/register', sanitizeUserInput, add);
 userRouter.put('/:id', sanitizeUserInput, update);
 userRouter.patch('/:id', sanitizeUserInput, update);
 userRouter.delete('/:id', remove);
+
+userRouter.post('/login', sanitizeUserInput, findOne);
+userRouter.post('/register', sanitizeUserInput, add);
+
+userRouter.get('/managers', findAllManagers);
+userRouter.get('/managers/:id', sanitizeUserInput, findOneManager);
+
+
