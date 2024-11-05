c2edb904895025d0d4af17e63d950bb3888cb42b
Santino S <santinosimbel@gmail.com>
2024-11-05 00:57:22 -0300

    user.routers reordenamiento de rutas para evitar conflictos

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -12,16 +12,17 @@ import {
 
 export const userRouter = Router();
 
+// No cambiar el orden de las rutas
+userRouter.post('/login', sanitizeUserInput, findOne);
+userRouter.post('/register', sanitizeUserInput, add);
+
+userRouter.get('/managers', findAllManagers);
+userRouter.get('/managers/:id', sanitizeUserInput, findOneManager);
 
 userRouter.get('/', findAll);
 userRouter.put('/:id', sanitizeUserInput, update);
 userRouter.patch('/:id', sanitizeUserInput, update);
 userRouter.delete('/:id', remove);
 
-userRouter.post('/login', sanitizeUserInput, findOne);
-userRouter.post('/register', sanitizeUserInput, add);
-
-userRouter.get('/managers', findAllManagers);
-userRouter.get('/managers/:id', sanitizeUserInput, findOneManager);
 
 
