0823da397ccfd8347df2cd02660a41420b07903c
Santino S <santinosimbel@gmail.com>
2024-12-28 20:00:20 -0300

    autorizacion rutas de launguage, movie, show

--- src/language/language.routes.ts
+++ src/language/language.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { sanitizeLanguageInput, findAll, findOne, add, remove } from "./language.controler.js";
+import { authMiddleware } from "../utils/authMiddleware.js";
 
 export const languageRouter = Router()
 
 languageRouter.get('/', findAll)
 languageRouter.get('/:id', findOne)
-languageRouter.post('/', sanitizeLanguageInput, add)
-languageRouter.delete('/:id', remove)
+languageRouter.post('/', authMiddleware(['manager']), sanitizeLanguageInput, add)
+languageRouter.delete('/:id', authMiddleware(['manager']), remove)
